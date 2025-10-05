4f7c92d48afb4d1fa8a2ffcc428d614f2addc7e8
PogoreltsevaAnnaUKDIPZ <anna.i.pohoreltseva@ukd.edu.ua>
2025-10-06 01:43:21 +0300

    Add files via upload

---
+++ practiceWork4.js
@@ -0,0 +1,48 @@
+// Завдання 1
+
+function functionForAddingNumbersUntilN(n, i = 0){
+    if(i === 5){
+        return i;
+    }
+    console.log(i + 1)
+    return functionForAddingNumbersUntilN(n, i + 1)
+}
+
+// Завдання 2
+
+function funcForAscendingAndDescendingAccordingToA(a, b){
+    if(a < b){
+        if(a === b){
+            console.log(a);
+         return a;
+        }
+        console.log(a);
+        return funcForAscendingAndDescendingAccordingToA(a + 1, b);
+    }
+    if(a > b){
+        if(b === a){
+            console.log(b)
+            return b;
+        }
+        console.log(a);
+        return funcForAscendingAndDescendingAccordingToA(a - 1, b);
+    }
+}
+
+// funcForAscendingAndDescendingAccordingToA(16, 5)
+
+// Завдання 3
+
+function funcActualGradeOfTwo(n, i = 0){}
+
+
+// Завдання 4
+
+function funcForSumOfIntegersInNumber(n) {
+    if (n < 10) {
+        return n;
+    }
+    return (n % 10) + funcForSumOfIntegersInNumber(Math.floor(n / 10));
+}
+
+console.log(funcForSumOfIntegersInNumber(567))
